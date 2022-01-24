80b7aeac75e53deca96f28fde6badc7a50e71387
proars <96272922+proars@users.noreply.github.com>
2022-01-23 18:34:36 -0800

    Add files via upload

---
+++ user_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum -y update
+yum -y install httpd
+service httpd start
+chkconfig httpd on
+echo "Hello $(hostname -f), Build by Terraform using external file" > /var/www/html/index.html
