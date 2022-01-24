fa6f30be1a862ced60394b812b47cd7f9c09526e
proars <96272922+proars@users.noreply.github.com>
2022-01-23 18:47:48 -0800

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -3,4 +3,4 @@ yum -y update
 yum -y install httpd
 service httpd start
 chkconfig httpd on
-echo "Hello $(hostname -f), Build by Terraform using external file" > /var/www/html/index.html
+echo "Hello $(hostname -f), Build by Terraform Cloud using external file" > /var/www/html/index.html
